f0cdccad8c8477accf5dc180aba6585f44646509
Aashish BishowKarma <aashishbishowkarma@outlook.com>
2025-01-31 05:04:56 +0545

    Add Calender

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { Calendar } from "@/components/ui/calendar"
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card"
+import React from "react";
 
 
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -17,6 +20,12 @@ export default function Home() {
               Hello, Visitor👋
             </HoverCardContent>
         </HoverCard>
+        <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
       </main>
     </div>
   );
