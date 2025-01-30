9ce4ff569a145ffd6fd25d7ab9becf3c802972fb
Aashish BishowKarma <aashishbishowkarma@outlook.com>
2025-01-31 05:35:48 +0545

    Fix Calender

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import React from "react";
+import { Calendar } from "@/components/ui/calendar";
 
 import {
   Carousel,
@@ -16,6 +17,7 @@ import {
 } from "@/components/ui/carousel";
 
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -23,6 +25,13 @@ export default function Home() {
           <HoverCardTrigger>Click me!</HoverCardTrigger>
           <HoverCardContent>Hello, Visitor👋</HoverCardContent>
         </HoverCard>
+
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
+        />
 
         <Carousel>
           <CarouselContent>
